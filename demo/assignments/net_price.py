ce1848e8bfff5086cd585274511940d00d73c0e0
Srikanth Pragada <srikanthpragada@gmail.com>
2025-09-26 09:06:12 +0530

    26-SEP

---
+++ demo/assignments/net_price.py
@@ -0,0 +1,12 @@
+# Take price and display net price after a discount of 15%
+
+price = int(input("Enter price :"))
+discount = price * 15 // 100
+net_price = price - discount
+
+print(f'Price     : {price:6}')
+print(f'Discount  : {discount:6}')
+print(f'Net price : {net_price:6}')
+
+
+
---
+++ demo/for_demo.py
@@ -0,0 +1,4 @@
+# Print 1 to 10 using for loop
+
+for n in range(1, 11):
+    print(n)
